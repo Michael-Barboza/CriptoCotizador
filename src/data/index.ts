19f9f48294f9cc942cd1ae6bab437d6656f42ecf
Michael-Barboza <michaelalexanderbarboza@outlook.com>
2025-07-31 12:03:38 -0300

    primer commit

---
+++ src/data/index.ts
@@ -0,0 +1,27 @@
+import { Currency } from "../types";
+
+export const currencies: Currency[] = [
+  { code: 'USD', name: 'Dólar de Estados Unidos' },
+  { code: 'MXN', name: 'Peso Mexicano' },
+  { code: 'EUR', name: 'Euro' },
+  { code: 'GBP', name: 'Libra Esterlina' },
+  { code: 'JPY', name: 'Yen Japonés' },
+  { code: 'AUD', name: 'Dólar Australiano' },
+  { code: 'CAD', name: 'Dólar Canadiense' },
+  { code: 'CHF', name: 'Franco Suizo' },
+  { code: 'CNY', name: 'Yuan Chino' },
+  { code: 'INR', name: 'Rupia India' },
+  { code: 'BRL', name: 'Real Brasileño' },
+  { code: 'ZAR', name: 'Rand Sudafricano' },
+  { code: 'HKD', name: 'Dólar de Hong Kong' },
+  { code: 'NZD', name: 'Dólar Neozelandés' },
+  { code: 'SEK', name: 'Corona Sueca' },
+  { code: 'NOK', name: 'Corona Noruega' },
+  { code: 'SGD', name: 'Dólar de Singapur' },
+  { code: 'ARS', name: 'Peso Argentino' }, // Moneda de Argentina
+  { code: 'CLP', name: 'Peso Chileno' }, // Moneda de Chile
+  { code: 'COP', name: 'Peso Colombiano' }, // Moneda de Colombia
+  { code: 'PEN', name: 'Sol Peruano' }, // Moneda de Perú
+  { code: 'UYU', name: 'Peso Uruguayo' }, // Moneda de Uruguay
+  { code: 'VEF', name: 'Bolívar Venezolano' } // Moneda de Venezuela
+];
